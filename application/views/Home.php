c96967d3d4c2f1af10c20ef4551a269c56a82131
Adoniseq <szumarat1>
2016-12-29 04:11:11 +0100

    Keeping going
    Index page with discounts row

--- application/views/Home.php
+++ application/views/Home.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet">
 </head>
 <body>
 <div class="container-fluid slider-background">
@@ -9,9 +10,12 @@
 
 	</div>
 </div>
-<div class="container">
+<div class="container-fluid" style="background-color: #f6f6f6;">
+	<div class="container">
 	<div class="row">
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
+	<header>PROMOCJE</header>
+		<?php for ($i=1; $i <=12 ; $i++) { ?>
+			<div class="col-sm-4 col-md-3" style="padding: 5px;">
 			<div class="product-data-box">
 				<div class="product-data-photo">
 					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
@@ -24,151 +28,14 @@
 				</div>
 			</div>
 		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
-		<div class="col-sm-4 col-md-3" style="padding: 5px;">
-			<div class="product-data-box">
-				<div class="product-data-photo">
-					<img src="http://efabrykamocy.pl/userdata/gfx/cdf2bc18b08fffcfe2e1d63e2b2e26a4.jpg" width="100%" height="120%">
-					<div class="discount">
-					-5%
-					</div>
-				</div>
-				<div class="product-data-title">
-					RĘKAWICE BOKSERSKIE ADIDAS PRO AIR
-				</div>
-			</div>
-		</div>
+		<?php } ?>
 		
 	</div>
+
+</div>
+</div>
+
+<div class="container-fluid" style="background-color: #494949; height: 200px;">
 
 </div>
 </body>
